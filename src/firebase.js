dad52ce2b48291336e40295ab19bff9be6c9c1f0
Hassan Ndam <ndamfiverr@gmail.com>
2024-01-24 17:26:18 +0100

    Update firebase account to h.ndam@apssafrance.com

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,27 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
 
 
 // Config API Firebase - Connexion BDD
+// Import the functions you need from the SDKs you need
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCAuic_gzFgFUy6nzHClxnOuavTRKkx9PM",
-    authDomain: "react-contact-7e55c.firebaseapp.com",
-    databaseURL: "https://react-contact-7e55c-default-rtdb.firebaseio.com/",
-    projectId: "react-contact-7e55c",
-    storageBucket: "react-contact-7e55c.appspot.com",
-    messagingSenderId: "692238819484",
-    appId: "1:692238819484:web:53c1692c92799435fe8d59"
+  apiKey: "AIzaSyARGwwKNHarc7pvEjut2ofG7RsvXsBB0X8",
+  authDomain: "contact-application-a319f.firebaseapp.com",
+  databaseURL: "https://contact-application-a319f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "contact-application-a319f",
+  storageBucket: "contact-application-a319f.appspot.com",
+  messagingSenderId: "1020101326440",
+  appId: "1:1020101326440:web:d6aabf276832e7d53d2ddd"
+};
 
-  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  const fireDb = firebase.initializeApp(firebaseConfig); 
-  export default fireDb.database().ref(); 
+const fireDb = firebase.initializeApp(firebaseConfig);
+export default fireDb.database().ref(); 
